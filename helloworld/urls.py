975ce78abc587cfd89dbc29741a5c2a78b94b485
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-21 14:35:17 +0300

    Добавил маршрут в helloworld/urls.py, к представлению hello_world

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django_api.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_world, name='django_hello_world')
 ]
