fe04d6928e77830a2a88c5c8a3afe8af6c5aa910
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-21 14:49:52 +0300

    Создал представление hello_world_drf

--- django_api/views.py
+++ django_api/views.py
@@ -1,5 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view()
+def hello_world_drf(request):
+    return Response({'message': 'hello world'})
 
 def hello_world(request):
     return JsonResponse({'message': 'hello world'})
